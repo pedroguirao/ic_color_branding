effc603e3ebe750218b3da17c8cc83fab63f14c1
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2019-09-13 08:02:40 +0000

    NEW REPO

---
+++ ic_black_branding/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "ic_black_brand",
+    'support': 'pedro.guirao@ingenieriacloud.com',
+
+    'summary': """
+        """,
+
+    'description': """
+   
+    """,
+    
+    'author': "ingenieriacloud.com",
+    'website': "http://www.ingenieriacloud.com",
+    'category': 'web',
+    'images': ['static/description/main.png'],
+    'version': '12.0.1',
+    'depends': ['web_enterprise', 'web'],
+
+    'data': [
+        'views/web_assets_backend.xml'
+    ],
+    'qweb': [
+        "static/src/xml/base.xml",
+    ],
+    'installable': True,
+}
